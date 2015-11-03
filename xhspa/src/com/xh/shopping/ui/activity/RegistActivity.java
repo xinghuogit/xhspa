5d1a8775c0f2439b1ff7e985cd5c4809bb2d568e
Administrator <Administrator@PC201509022221>
2015-11-03 22:07:42 +0800

    上传项目

---
+++ xhspa/src/com/xh/shopping/ui/activity/RegistActivity.java
@@ -0,0 +1,36 @@
+/*************************************************************************************************
+ * 版权所有 (C)2015,  
+ * 
+ * 文件名称：RegisterActivity.java
+ * 内容摘要：RegisterActivity.java
+ * 当前版本：TODO
+ * 作        者：李加蒙
+ * 完成日期：2015-11-3 下午9:35:16
+ * 修改记录：
+ * 修改日期：2015-11-3 下午9:35:16
+ * 版   本 号：
+ * 修   改 人：
+ * 修改内容：
+ ************************************************************************************************/
+package com.xh.shopping.ui.activity;
+
+import com.xh.shopping.ui.activity.fragment.RegistFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * @FileName 文件名称：RegisterActivity.java
+ * @Content 内容摘要：注册界面
+ */
+public class RegistActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		RegistFragment registFragment = new RegistFragment();
+		FragmentManager manager = registFragment.getFragmentManager();
+	}
+
+}
---
+++ xhspa/src/com/xh/shopping/ui/activity/fragment/RegistFragment.java
@@ -0,0 +1,50 @@
+/*************************************************************************************************
+ * 版权所有 (C)2015,  
+ * 
+ * 文件名称：RegistFragment.java
+ * 内容摘要：RegistFragment.java
+ * 当前版本：TODO
+ * 作        者：李加蒙
+ * 完成日期：2015-11-3 下午9:44:58
+ * 修改记录：
+ * 修改日期：2015-11-3 下午9:44:58
+ * 版   本 号：
+ * 修   改 人：
+ * 修改内容：
+ ************************************************************************************************/
+package com.xh.shopping.ui.activity.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ @filename文件名称：RegistFragment.java
+ @content
+ */
+/**
+ * @FileName 文件名称：RegistFragment.java
+ * @Content 内容摘要：注册 Fragment
+ */
+public class RegistFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+}
